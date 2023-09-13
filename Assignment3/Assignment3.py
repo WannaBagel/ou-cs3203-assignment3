519b77aa05f1f6e05e68d67f865617233607db70
WannaBagel <lcagle101@gmail.com>
2023-09-13 17:28:33 -0500

    Task 3 commit

--- Assignment3/Assignment3.py
+++ Assignment3/Assignment3.py
@@ -1,14 +1,34 @@
-theList = [ 1,3,5,7,9 ]
+from itertools import product
+
+
 
 def adder(theList):
     temp = 0
     for i in range(len(theList)):
         temp = temp + theList[i]
-    print(temp)
-adder(theList)
+    return(temp)
 def productFinder(list):
     temp = 1
     for i in range(len(list)):
         temp = temp * list[i]
     return temp
-print(productFinder(theList))
+def main():
+    print("Welcome to assignment 3 \n")
+    ourList = []
+    while True:
+        userInput = input("Insert a number, or 'Stop' to end \n")
+        
+        if userInput.lower() == "stop":
+            break
+        
+        try:
+            number = int(userInput)
+            ourList.append(number)
+        except ValueError:
+            print("Invalid input. Please enter a valid number or 'stop' to stop. \n")
+    user_choice = input("Type 'Adder' for addition or 'mult' for multiplication.' \n")
+    
+    print(adder(ourList))
+    print(productFinder(ourList))
+        
+main()
