5e5646af2316b57c272398251d16fd5507f602eb
WannaBagel <lcagle101@gmail.com>
2023-09-13 23:06:19 -0500

    Test cmommit

--- Assignment3/Assignment3.py
+++ Assignment3/Assignment3.py
@@ -37,5 +37,6 @@ def main():
     print(adder(ourList))
     print(productFinder(ourList))
     print(reverse(ourList)) #Comment for to add additional commit
-        
+        #comment for merge
+
 main()
